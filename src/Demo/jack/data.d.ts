a3350c7aa0d99859ca4e3b3cbb569076b3075644
zhenglipeng <zhenglipeng@sensetime.com>
2020-11-17 14:31:18 +0800

    补充swagger结构定义

--- src/Demo/jack/data.d.ts
+++ src/Demo/jack/data.d.ts
@@ -93,29 +93,96 @@ export interface ExternalDocumentationObject {
   description: string;  // 目标文档的简短描述。 CommonMark语法可以用于富文本表示。
 }
 
-export interface TagObject {
-  name: string;           // tag名称
-  description?: string;   // 标签的简短说明。 CommonMark语法可以用于富文本表示。
-  externalDocs?: ExternalDocumentationObject;  // 此标记的其他外部文档。
-}
-
 export interface ParameterObject {
   /**
-   * REQUIRED. The name of the parameter. Parameter names are case sensitive.
-If in is "path", the name field MUST correspond to a template expression occurring within the path field in the Paths Object. See Path Templating for further information.
-If in is "header" and the name field is "Accept", "Content-Type" or "Authorization", the parameter definition SHALL be ignored.
-For all other cases, the name corresponds to the parameter name used by the in property.
+   * 必需。参数名称。大小写敏感。
+   * 如果in==="path"，那么name字段必须对应于在Paths对象的path字段中出现的模板表达式，有关详细信息，请参阅路径模板。
+   * 如果in==="header"，而name字段是"Accept"、"Content-Type"或"Authorization"，则忽略参数定义。
+   * 对于所有其他情况，名称对应于in属性使用的参数名称。
    */
   name: string;
   in: string;            // 必需。参数的位置。可能的值为"query", "header", "path" 或 "cookie".
   description?: string;  // 参数的简要说明。这可能包含使用示例。 CommonMark语法可以用于富文本表示。
   required?: boolean;    // 确定此参数是否为必需。如果参数位置是“ path”，则此属性是必需的，其值必须为true。否则，可以包括该属性，并且其默认值为false。
   deprecated?: boolean;  // 指定不推荐使用参数，并且应该过渡使用。默认值为false。
-  allowEmptyValue?: boolean;  // Sets the ability to pass empty-valued parameters. This is valid only for query parameters and allows sending a parameter with an empty value. Default value is false. If style is used, and if behavior is n/a (cannot be serialized), the value of allowEmptyValue SHALL be ignored. Use of this property is NOT RECOMMENDED, as it is likely to be removed in a later revision.
+  allowEmptyValue?: boolean;  // 设置传递空值参数的能力。这只对查询参数有效，并允许发送带有空值的参数。默认值为false。如果使用样式，如果行为是n/a(不能序列化)，allowEmptyValue的值将被忽略。不建议使用此属性，因为它很可能在以后的修订中被删除。
+}
+
+export interface RequestBodyObject {
+  content: {[key: string]: MediaTypeObject};  // 必需。请求正文的内容。关键字是媒体类型或媒体类型范围，值描述了它。对于与多个键匹配的请求，仅最特定的键适用。例如"text/plain"取代"text/*""
+  description?: string;                       // 请求正文的简短描述。这可能包含使用示例。 CommonMark语法可以用于富文本表示。
+  required?: boolean;                         // 确定请求中是否需要请求主体。默认为false。
+}
+
+export interface MediaTypeObject {
+  schema?: SchemaObject | ReferenceObject;   // 定义请求，响应或参数的内容的模式。
+  example?: any;                             // 媒体类型的示例。示例对象应该采用媒体类型指定的正确格式。"example"字段与"examples"字段互斥。此外，如果引用包含示例的模式，则"examples"值将覆盖该模式提供的"example"。
+  examples?: {[key: string]: ExampleObject | ReferenceObject}; // Examples of the media type. Each example object SHOULD match the media type and specified schema if present. The examples field is mutually exclusive of the example field. Furthermore, if referencing a schema which contains an example, the examples value SHALL override the example provided by the schema.
+  encoding?: {[key: string]: EncodingObject};  // A map between a property name and its encoding information. The key, being the property name, MUST exist in the schema as a property. The encoding object SHALL only apply to requestBody objects when the media type is multipart or application/x-www-form-urlencoded.
+}
+
+export interface EncodingObject {
+  contentType: string;  // The Content-Type for encoding a specific property. Default value depends on the property type: for string with format being binary – application/octet-stream; for other primitive types – text/plain; for object - application/json; for array – the default is defined based on the inner type. The value can be a specific media type (e.g. application/json), a wildcard media type (e.g. image/*), or a comma-separated list of the two types.
+  headers: {[key: string]: HeaderObject | ReferenceObject};   // A map allowing additional information to be provided as headers, for example Content-Disposition. Content-Type is described separately and SHALL be ignored in this section. This property SHALL be ignored if the request body media type is not a multipart.
+  style: string;  // Describes how a specific property value will be serialized depending on its type. See Parameter Object for details on the style property. The behavior follows the same values as query parameters, including default values. This property SHALL be ignored if the request body media type is not application/x-www-form-urlencoded.
+  explode: boolean; // When this is true, property values of type array or object generate separate parameters for each value of the array, or key-value-pair of the map. For other types of properties this property has no effect. When style is form, the default value is true. For all other styles, the default value is false. This property SHALL be ignored if the request body media type is not application/x-www-form-urlencoded.
+  allowReserved: boolean; // Determines whether the parameter value SHOULD allow reserved characters, as defined by RFC3986 :/?#[]@!$&'()*+,;= to be included without percent-encoding. The default value is false. This property SHALL be ignored if the request body media type is not application/x-www-form-urlencoded.
+}
+
+export interface ResponsesObject {
+  default: ResponseObject | ReferenceObject;                  // The documentation of responses other than the ones declared for specific HTTP response codes. Use this field to cover undeclared responses. A Reference Object can link to a response that the OpenAPI Object's components/responses section defines.
+  [HTTPStatusCode: number]: ResponseObject | ReferenceObject; // Any HTTP status code can be used as the property name, but only one property per code, to describe the expected response for that HTTP status code. A Reference Object can link to a response that is defined in the OpenAPI Object's components/responses section. This field MUST be enclosed in quotation marks (for example, "200") for compatibility between JSON and YAML. To define a range of response codes, this field MAY contain the uppercase wildcard character X. For example, 2XX represents all response codes between [200-299]. Only the following range definitions are allowed: 1XX, 2XX, 3XX, 4XX, and 5XX. If a response is defined using an explicit code, the explicit code definition takes precedence over the range definition for that code.
+}
+
+export interface ResponseObject {
+  description: string;    // REQUIRED. A short description of the response. CommonMark syntax MAY be used for rich text representation.
+  headers: {[key: string]: HeaderObject | ReferenceObject}; // Maps a header name to its definition. RFC7230 states header names are case insensitive. If a response header is defined with the name "Content-Type", it SHALL be ignored.
+  content: {[key: string]: MediaTypeObject};  // A map containing descriptions of potential response payloads. The key is a media type or media type range and the value describes it. For responses that match multiple keys, only the most specific key is applicable. e.g. text/plain overrides text/*
+  links: {[key: string]: LinkObject | ReferenceObject}; // A map of operations links that can be followed from the response. The key of the map is a short name for the link, following the naming constraints of the names for Component Objects.
+}
+
+export interface CallbackObject {
+  [expression: string]: PathItemObject;   // A Path Item Object used to define a callback request and expected responses. A complete example is available.
+}
+
+export interface ExampleObject {
+  summary: string;        // Short description for the example.
+  description: string;    // Long description for the example. CommonMark syntax MAY be used for rich text representation.
+  value: any;             // Embedded literal example. The value field and externalValue field are mutually exclusive. To represent examples of media types that cannot naturally represented in JSON or YAML, use a string value to contain the example, escaping where necessary.
+  externalValue: string;  // A URL that points to the literal example. This provides the capability to reference examples that cannot easily be included in JSON or YAML documents. The value field and externalValue field are mutually exclusive.
+}
+
+export interface LinkObject {
+  operationRef: string;       // A relative or absolute URI reference to an OAS operation. This field is mutually exclusive of the operationId field, and MUST point to an Operation Object. Relative operationRef values MAY be used to locate an existing Operation Object in the OpenAPI definition.
+  operationId: string;        // The name of an existing, resolvable OAS operation, as defined with a unique operationId. This field is mutually exclusive of the operationRef field.
+  parameters: {[key: string]: any | {expression: string}}   // A map representing parameters to pass to an operation as specified with operationId or identified via operationRef. The key is the parameter name to be used, whereas the value can be a constant or an expression to be evaluated and passed to the linked operation. The parameter name can be qualified using the parameter location [{in}.]{name} for operations that use the same parameter name in different locations (e.g. path.id).
+  requestBody: any | {expression: string};  // A literal value or {expression} to use as a request body when calling the target operation.
+  description: string;        // A description of the link. CommonMark syntax MAY be used for rich text representation.
+  server: ServerObject;       // A server object to be used by the target operation.
+}
+
+/**
+ * The Header Object follows the structure of the ParameterObject with the following changes:
+ * 1. name MUST NOT be specified, it is given in the corresponding headers map.
+ * 2. in MUST NOT be specified, it is implicitly in header.
+ * 3. All traits that are affected by the location MUST be applicable to a location of header (for example, style).
+ */
+export interface HeaderObject extends ParameterObject {
+
+}
+
+export interface TagObject {
+  name: string;           // tag名称
+  description?: string;   // 标签的简短说明。 CommonMark语法可以用于富文本表示。
+  externalDocs?: ExternalDocumentationObject;  // 此标记的其他外部文档。
 }
 
 export interface ReferenceObject {
   $ref: string; // 必需。引用链接
+}
+
+export interface SchemaObject {
+
 }
 
 export interface ServerObject {
@@ -132,4 +199,45 @@ export interface ServerVariableObject {
   enum?: string[];      // 如果替换选项来自有限集合，则使用字符串值的枚举。数组不应为空。
   default: string;      // 必需。用于替代的默认值，如果未提供替代值，则应发送该默认值。注意，此行为不同于模式对象对默认值的处理，因为在这种情况下，参数值是可选的。如果定义了枚举，则该枚举的值中应该存在该值。
   description?: string; // 服务器变量的可选描述。CommonMark语法可以用于富文本表示。
+}
+
+export interface ComponentsObject {
+  schemas: {[key: string]: SchemaObject | ReferenceObject}; // An object to hold reusable Schema Objects.
+  responses: {[key: string]: SchemaObject | ReferenceObject}; // An object to hold reusable Response Objects.
+  parameters: {[key: string]: ParameterObject | ReferenceObject}; // An object to hold reusable Parameter Objects.
+  examples: {[key: string]: ExampleObject | ReferenceObject}; // An object to hold reusable Example Objects.
+  requestBodies: {[key: string]: RequestBodyObject | ReferenceObject}; //	An object to hold reusable Request Body Objects.
+  headers: {[key: string]: HeaderObject | ReferenceObject}; // An object to hold reusable Header Objects.
+  securitySchemes: {[key: string]: SecuritySchemeObject | ReferenceObject}; // An object to hold reusable Security Scheme Objects.
+  links: {[key: string]: LinkObject | ReferenceObject}; // An object to hold reusable Link Objects.
+  callbacks: {[key: string]: CallbackObject | ReferenceObject}; // An object to hold reusable Callback Objects.
+}
+
+export interface SecuritySchemeObject {
+  type: string;             // Any	REQUIRED. The type of the security scheme. Valid values are "apiKey", "http", "oauth2", "openIdConnect".
+  description: string;      // Any	A short description for security scheme. CommonMark syntax MAY be used for rich text representation.
+  name: string;             // apiKey	REQUIRED. The name of the header, query or cookie parameter to be used.
+  in: string;               // apiKey	REQUIRED. The location of the API key. Valid values are "query", "header" or "cookie".
+  scheme: string;           // http	REQUIRED. The name of the HTTP Authorization scheme to be used in the Authorization header as defined in RFC7235. The values used SHOULD be registered in the IANA Authentication Scheme registry.
+  bearerFormat: string;     // http ("bearer")	A hint to the client to identify how the bearer token is formatted. Bearer tokens are usually generated by an authorization server, so this information is primarily for documentation purposes.
+  flows: OAuthFlowsObject;  // oauth2	REQUIRED. An object containing configuration information for the flow types supported.
+  openIdConnectUrl: string; // openIdConnect	REQUIRED. OpenId Connect URL to discover OAuth2 configuration values. This MUST be in the form of a URL.
+}
+
+export interface OAuthFlowsObject {
+  implicit: OAuthFlowObject;  // Configuration for the OAuth Implicit flow
+  password: OAuthFlowObject;  // Configuration for the OAuth Resource Owner Password flow
+  clientCredentials: OAuthFlowObject;  // Configuration for the OAuth Client Credentials flow. Previously called application in OpenAPI 2.0.
+  authorizationCode: OAuthFlowObject;  // Configuration for the OAuth Authorization Code flow. Previously called accessCode in OpenAPI 2.0.
+}
+
+export interface OAuthFlowObject {
+  authorizationUrl: string;         // oauth2 ("implicit", "authorizationCode")	REQUIRED. The authorization URL to be used for this flow. This MUST be in the form of a URL.
+  tokenUrl: string;                 // oauth2 ("password", "clientCredentials", "authorizationCode")	REQUIRED. The token URL to be used for this flow. This MUST be in the form of a URL.
+  refreshUrl: string;               // oauth2	The URL to be used for obtaining refresh tokens. This MUST be in the form of a URL.
+  scopes: {[key: string]: string};  // oauth2	REQUIRED. The available scopes for the OAuth2 security scheme. A map between the scope name and a short description for it. The map MAY be empty.
+}
+
+export interface SecurityRequirementObject {
+  [name: string]: string; // Each name MUST correspond to a security scheme which is declared in the Security Schemes under the Components Object. If the security scheme is of type "oauth2" or "openIdConnect", then the value is a list of scope names required for the execution, and the list MAY be empty if authorization does not require a specified scope. For other security scheme types, the array MUST be empty.
 }
--- src/Demo/jack/index.ts
+++ src/Demo/jack/index.ts
@@ -1,7 +1,7 @@
 import request from './request';
 import { toCamelCase } from './utils';
 
-import { SwaggerConfig, ParameterType } from './data';
+import { SwaggerConfig, ParameterObject } from './data';
 
 export default class Jack {
   info: any;
@@ -32,14 +32,14 @@ export default class Jack {
       value: async (args: any) => {
         if (cfg.parameters) {
           // 参数验证
-          cfg.parameters.forEach((param: ParameterType) => {
+          cfg.parameters.forEach((param: ParameterObject) => {
             if (param.required && typeof args[param.name] === 'undefined') {
               throw new Error(`${param.name}参数不能为空 ${param.description || ''}`);
             }
           });
 
           // path处理
-          cfg.parameters.forEach((param: ParameterType) => {
+          cfg.parameters.forEach((param: ParameterObject) => {
             if (param.in === 'path') {
               path = path.replace(`{${param.name}}`, args[param.name]);
             }
